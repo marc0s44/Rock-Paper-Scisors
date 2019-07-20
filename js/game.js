53c303096bca3c8e571047f2411e1d7d48424892
Marek Odrzywolek <marcosxd44@gmail.com>
2019-07-20 17:07:14 +0200

    Change start button to reset button and add JS code to make the game work

--- js/game.js
+++ js/game.js
@@ -1,15 +1,50 @@
-function playRound () {
-    let numberOfRounds = prompt("Enter how many rounds you want to play");
-    for(let i = 0;i<numberOfRounds;i++) {
-    console.log(computerPlays());
-    }
-}
-
+let playerScore = 0;
+let computerScore = 0;
+const winningScore = 5;
 function computerPlays() {
     let randomNumber = Math.floor(Math.random() * 3 + 1);
     if(randomNumber === 1) return "rock";
     else if(randomNumber=== 2) return "paper";
     else return "scissors";
 }
-const startButton = document.getElementById("startGameButton");
-startButton.addEventListener("click", playRound);
+function playRound(e) {
+    if(computerScore<5 && playerScore<5){
+    let playerGuess = e.target.value;
+    let computerGuess = computerPlays();
+    console.log("Computer: " + computerGuess);
+    console.log("Player: "+ playerGuess);
+    if(playerGuess===computerGuess) {
+        console.log("Draw");
+    }
+    else if(playerGuess==="rock" && computerGuess==="scissors" || playerGuess==="paper" && computerGuess==="rock" || 
+    playerGuess==="scissors" && computerGuess==="paper") {
+        playerScore++;
+        console.log("You won");
+    }
+    else {
+        computerScore++;
+        console.log("Computer won");
+    }
+    checkWinner();
+}
+
+}
+const resetButton = document.getElementById("startGameButton");
+resetButton.addEventListener("click",()=> {
+    playerScore = 0;
+    computerScore = 0;
+});
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+rockButton.addEventListener("click", playRound);
+paperButton.addEventListener("click", playRound);
+scissorsButton.addEventListener("click", playRound);
+function checkWinner() {
+    if(computerScore==winningScore) {
+    console.log("Computer won the game");
+}
+else if (playerScore==winningScore) {
+    console.log("Player won the game");
+}
+}
